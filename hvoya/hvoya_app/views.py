7e85b1eaa8fc9226aa4bb1afdc348920a7411898
freak-js <mahrov.web@gmail.com>
2020-07-04 14:14:31 +0300

    Добавлена кастомная страница 403 ошибки, функционал распределен по модулям.

--- hvoya/hvoya_app/views.py
+++ hvoya/hvoya_app/views.py
@@ -1,54 +1,20 @@
-import json
 from datetime import datetime, timedelta
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-from .models import GrowBox, GrowBoxDateTime, GrowBoxHistoricalData, GrowBoxSettings
+from .models import GrowBoxDateTime, GrowBoxHistoricalData
+from .utils.db_utils import get_historical_data
+from .utils.token_auth import token_checker
 
 
+@token_checker
 def index(request: HttpRequest) -> HttpResponse:
     """
 
     :param request:
     :return: HttpResponse
     """
-    def get_historical_data():
-        air_temperature = []
-        air_humidity = []
-        soil_humidity = []
-        yesterday_air_temperature = []
-        yesterday_air_humidity = []
-        yesterday_soil_humidity = []
-
-        current_datetime: datetime = datetime.now()
-        historical_data_queryset = GrowBoxHistoricalData.objects.select_related().filter(
-            datetime__year=current_datetime.year,
-            datetime__month=current_datetime.month,
-            datetime__day__gte=current_datetime.day - 1
-        ).order_by('id')
-
-        for data in historical_data_queryset:
-
-            if data.datetime.day == current_datetime.day:
-                air_temperature.append(data.air_temperature)
-                air_humidity.append(data.air_humidity)
-                soil_humidity.append(data.soil_humidity)
-            else:
-                yesterday_air_temperature.append(data.air_temperature)
-                yesterday_air_humidity.append(data.air_humidity)
-                yesterday_soil_humidity.append(data.soil_humidity)
-
-        historical_data: dict = {
-            'air_temperature': air_temperature,
-            'air_humidity': air_humidity,
-            'soil_humidity': soil_humidity,
-            'yesterday_air_temperature': yesterday_air_temperature,
-            'yesterday_air_humidity': yesterday_air_humidity,
-            'yesterday_soil_humidity': yesterday_soil_humidity
-        }
-        return historical_data
-
     historical_data = get_historical_data()
     return render(request, 'hvoya_app/index.html', {'historical_data': historical_data})
 
