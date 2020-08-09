92b9e1324e54bd1a4c374dceaa530129cd65c197
freak-js <mahrov.web@gmail.com>
2020-08-09 21:55:21 +0300

    Диаграмма спелости теперь отрисовывается на основе реальных данных из настроек гроубокса.

--- hvoya/hvoya_app/models.py
+++ hvoya/hvoya_app/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class GrowBoxSettings(models.Model):
     minimal_soil_humidity = models.IntegerField('Минимальная влажность грунта в процентах')
     lamp_on_time = models.IntegerField('Время включения лампы в часах')
-    lamp_off_time = models.IntegerField('Время включения лампы в часах')
+    lamp_off_time = models.IntegerField('Время выключения лампы в часах')
     pump_run_time = models.IntegerField('Время работы помпы в секундах')
     data_sending_frequency = models.IntegerField('Таймер отправки данных с платы в секундах')
+    planting_date = models.CharField('Дата посадки в формате: 25.08.2020', max_length=20, null=True)
+    days_before_harvest = models.IntegerField('Дней до предполагаемой технической спелости', null=True)
 
 
 class GrowBoxDateTime(models.Model):
--- hvoya/hvoya_app/utils/settings_utils.py
+++ hvoya/hvoya_app/utils/settings_utils.py
@@ -1,6 +1,7 @@
 # Этот модуль предназначен для работы с настройками гроубокса
 
 from typing import Dict
+import datetime
 
 from django.http import HttpRequest
 from django.core.cache import cache
@@ -38,7 +39,8 @@ def set_default_settings_and_cash_them() -> None:
 def get_settings_data() -> Dict[str, int]:
     """
     Возвращает кеш настроек гроубокса в формате словаря.
-    Если настройки не найдены в Redis, то кеширует их.
+    Если настройки не найдены в Redis, то устанавливает
+    значения по умолчанию из модуля settings и кеширует их.
     """
     cashed_settings_data_value = get_cashed_settings_data().values()
 
@@ -62,6 +64,36 @@ def get_cashed_settings_data() -> Dict[str, int]:
     return cashed_settings_data
 
 
+def get_settings_for_donut_chart() -> dict:
+    """
+    Делает запрос в базу и получает данные для
+    формирования круговой диаграммы спелости.
+    """
+    growbox_settings: GrowBoxSettings = GrowBoxSettings.objects.all().first()
+    date_format = '%Y-%m-%d'
+
+    if not growbox_settings:
+        set_default_settings_and_cash_them()
+
+    planting_date = growbox_settings.planting_date
+    days_before_harvest = growbox_settings.days_before_harvest
+
+    planting_datetime = datetime.datetime.strptime(planting_date, date_format)
+    harvest_datetime = planting_datetime + datetime.timedelta(days=days_before_harvest)
+    now_datetime = datetime.datetime.now()
+
+    remaining_days = 0 if now_datetime >= harvest_datetime else (harvest_datetime - now_datetime).days
+    days_in_growth_stage = (now_datetime - planting_datetime).days
+
+    data_for_donut_char = {
+        'planting_date': planting_date,
+        'days_before_harvest': days_before_harvest,
+        'remaining_days': remaining_days,
+        'days_in_growth_stage': days_in_growth_stage
+    }
+    return data_for_donut_char
+
+
 def set_new_settings(request: HttpRequest) -> None:
     """
     Устанавливает новые настройки полученные от пользователя.
@@ -76,8 +108,18 @@ def set_new_settings(request: HttpRequest) -> None:
     lamp_off_time = request.POST.get('lamp_off_time')
     pump_run_time = request.POST.get('pump_run_time')
     data_sending_frequency = request.POST.get('data_sending_frequency')
+    planting_date = request.POST.get('planting_date')
+    days_before_harvest = request.POST.get('days_before_harvest')
 
-    if not all([minimal_soil_humidity, lamp_on_time, lamp_off_time, pump_run_time, data_sending_frequency]):
+    if not all([
+        minimal_soil_humidity,
+        lamp_on_time,
+        lamp_off_time,
+        pump_run_time,
+        data_sending_frequency,
+        planting_date,
+        days_before_harvest
+    ]):
         raise FieldError
 
     settings.minimal_soil_humidity = minimal_soil_humidity
@@ -85,6 +127,8 @@ def set_new_settings(request: HttpRequest) -> None:
     settings.lamp_off_time = lamp_off_time
     settings.pump_run_time = pump_run_time
     settings.data_sending_frequency = data_sending_frequency
+    settings.planting_date = planting_date
+    settings.days_before_harvest = days_before_harvest
     settings.save()
 
     cache.set('minimal_soil_humidity', minimal_soil_humidity, timeout=None)
